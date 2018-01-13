58dd2b3fffc6c9f84a2cc0c6680f6e5fac97cc5a
Ivan Driuk <veytarn@gmail.com>
2018-01-13 07:55:29 +0200

    class component with event hangler

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import keys from '../keys.js'
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = keys.youtube_api_key;
 
 // Create a new component. This component should produce
 // some html
 const App = () => {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
