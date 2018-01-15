3d335f4ad3497bea8732368429e2d1c814259db2
Ivan Driuk <veytarn@gmail.com>
2018-01-15 07:44:15 +0200

    redux promise and axios

--- rs3weather/src/actions/index.js
+++ rs3weather/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '79a75696684efc7388e79f74c79bca37';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
