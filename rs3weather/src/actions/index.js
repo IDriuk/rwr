05337178a80b52f3644a1fa634d034bea0b2adbd
Ivan Driuk <veytarn@gmail.com>
2018-01-15 18:59:44 +0200

    fix url, bind actions to component

--- rs3weather/src/actions/index.js
+++ rs3weather/src/actions/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-
 const API_KEY = '79a75696684efc7388e79f74c79bca37';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=79a75696684efc7388e79f74c79bca37&q=123,us
+${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  console.log(url);
   const request = axios.get(url);
 
   return {
