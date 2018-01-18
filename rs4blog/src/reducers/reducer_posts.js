3c0fd5ca7a3feddbfe57e282d7f84e3ff143640d
Ivan Driuk <veytarn@gmail.com>
2018-01-18 15:40:36 +0200

    receiving new posts

--- rs4blog/src/reducers/reducer_posts.js
+++ rs4blog/src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch(action.type) {
+    case FETCH_POST:
+        const { payload: { data }} = action;
+        return { ...state, [data.id]: data}
     case FETCH_POSTS:
         return _.mapKeys(action.payload.data, 'id');
     default:
