21489d528ccf2e8b1d47c5dd0be82f10effb21c6
Ivan Driuk <veytarn@gmail.com>
2018-01-18 15:57:10 +0200

    own props

--- rs4blog/src/components/posts_show.js
+++ rs4blog/src/components/posts_show.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { postFetch } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return (
       <div>
         Posts Show!
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  preturn { post: posts[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
